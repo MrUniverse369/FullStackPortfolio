820cfc7fa5d710bed006d8651c2380a59df676e5
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-09-19 17:12:40 +0100

    project page now features six projects all styled

--- FullStackPortfolio/src/Project.jsx
+++ FullStackPortfolio/src/Project.jsx
@@ -1,18 +1,61 @@
 import './Project.css'
 function Project(props){return(
-    <>
- <div className='ProjectsHolder'>
-    <h2>Projects</h2>
-    <div className='Project'>
-<div className='projectImg'><img src='./src/Azure.png'></img></div>
-<div><h5>Azure Horizon Inn </h5>
+
+ <div className='ProjectsContainer'>
+ <h2>Projects</h2>
+ <div className='ProjectItemOne'>
+ <img  className='projectImg' src='/images/Xylo.png'></img>
+<div><h5>Xylo Fullstack</h5>
+  <p>Xylo is a fullstack  website for a Fintech, featuring RESTful APIs and CRUD applications. The site allows users to  create accounts, shop for payment hardware and software, and request customer service/sale team. This was a solo project in which I personally implemented EJS, CSS (using Flexbox and Grid), JavaScript,Node.js Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
+     <a  href='https://xylo.ltd/' className='btn'>Live Demo</a>
+     <a className='btn'>GitHub</a>
+</div>
+
+</div>
+ <div className='ProjectItemTwo'>
+<img className='projectImg' src='/images/Set8JamsWeb.png'></img>
+<div><h5>Set8Jams React-Front-End </h5>
+  <p className=''>Azure Horizon is a fullstack React website for a hotel, featuring RESTful APIs and CRUD applications. The site allows users to create accounts, book and plan holidays, and manage events—all through a single-page React application. This was a solo project in which I personally implemented HTML, CSS (using Flexbox and Grid), JavaScript, Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
+     <a className='btn' href='https://mruniverse369.github.io/AzureHorizon/index.html'>Live Demo</a>
+     <a className='btn'>GitHub</a>
+</div>
+ </div>
+ <div className='ProjectItemThree'>
+<img className='projectImg' src='/images/HNLSandFour.png'></img>
+<div><h5>Harvey's Nine Lives </h5>
   <p className=''>Azure Horizon is a fullstack React website for a hotel, featuring RESTful APIs and CRUD applications. The site allows users to create accounts, book and plan holidays, and manage events—all through a single-page React application. This was a solo project in which I personally implemented HTML, CSS (using Flexbox and Grid), JavaScript, Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
      <a className='btn'>Live Demo</a>
      <a className='btn'>GitHub</a>
 </div>
+ </div>
+
+ <div className='ProjectItemFour'>
+<img className='projectImg' src='/images/GTCCover.png'></img>
+<div><h5>Galactic crusade</h5>
+  <p className=''>Azure Horizon is a fullstack React website for a hotel, featuring RESTful APIs and CRUD applications. The site allows users to create accounts, book and plan holidays, and manage events—all through a single-page React application. This was a solo project in which I personally implemented HTML, CSS (using Flexbox and Grid), JavaScript, Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
+     <a className='btn'>Live Demo</a>
+     <a className='btn'>GitHub</a>
 </div>
+ </div>
+
+ <div className='ProjectItemFive'>
+<img className='projectImg' src='/images/BattleShip.png'></img>
+<div><h5>BatttleShip </h5>
+  <p className=''>Azure Horizon is a fullstack React website for a hotel, featuring RESTful APIs and CRUD applications. The site allows users to create accounts, book and plan holidays, and manage events—all through a single-page React application. This was a solo project in which I personally implemented HTML, CSS (using Flexbox and Grid), JavaScript, Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
+     <a className='btn'>Live Demo</a>
+     <a className='btn'>GitHub</a>
 </div>
-    </>
+ </div>
+ <div className='ProjectItemSix'>
+<img className='projectImg' src='/images/SnakesAndLaddersTwo.png'></img>
+<div><h5>Snakes and Ladders </h5>
+  <p className=''>Azure Horizon is a fullstack React website for a hotel, featuring RESTful APIs and CRUD applications. The site allows users to create accounts, book and plan holidays, and manage events—all through a single-page React application. This was a solo project in which I personally implemented HTML, CSS (using Flexbox and Grid), JavaScript, Express.js REST APIs, and PostgreSQL to deliver a user-friendly, responsive, and visually appealing web experience</p>
+     <a className='btn'>Live Demo</a>
+     <a className='btn'>GitHub</a>
+</div>
+ </div>
+</div>
+
 )}
 
 export default Project
--- FullStackPortfolio/src/Skills.jsx
+++ FullStackPortfolio/src/Skills.jsx
@@ -7,22 +7,22 @@ function Skills(props){
    <h2><u>Skills</u></h2>
       <div className="SkillsTable"> 
       <div className="toolUsed"><h3>HTML</h3><img src="/images/html.png"></img></div>
-      <div className="toolUsed"><h3>CSS</h3><img src="./src/css.png"></img></div>
-      <div className="toolUsed"><h3>JavaScript</h3><img src="./src/javascript.png"></img></div>
-      <div className="toolUsed"><h3>React</h3><img src="./src/React.png"></img></div>
-      <div className="toolUsed"><h3>JQuery</h3><img src="./src/JQuery.png"></img></div>
-      <div className="toolUsed"><h3>EJS</h3><img src="./src/ejs.png"></img></div>
-      <div className="toolUsed"><h3>Node.js</h3><img src="./src/nodejs.png"></img></div>
-      <div className="toolUsed"><h3>Express.js</h3><img src="./src/expressjs.png"></img></div>
-      <div className="toolUsed"><h3>APIs/Rest Apis</h3><img src="./src/api.png"></img></div>
-      <div className="toolUsed"><h3>SQL</h3><img src="./src/sql.png"></img></div>
-      <div className="toolUsed"><h3>PostgreSQL</h3><img src="./src/postgresql.png"></img></div>
-      <div className="toolUsed"><h3>BOOTSTRAP</h3><img src="./src/bootstrap.png"></img></div>
-      <div className="toolUsed"><h3>C++</h3><img src="./src/Cplus.png"></img></div>
-      <div className="toolUsed"><h3>C#</h3><img src="./src/CSharp.png"></img></div>
-      <div className="toolUsed"><h3>OOP</h3><img src="./src/OOP.png"></img></div>
-      <div className="toolUsed"><h3>Unity</h3><img src="./src/unity.png"></img></div>
-      <div className="toolUsed"><h3>Git</h3><img src="./src/Git.png"></img></div>
+      <div className="toolUsed"><h3>CSS</h3><img src="/images/css.png"></img></div>
+      <div className="toolUsed"><h3>JavaScript</h3><img src="/images/javascript.png"></img></div>
+      <div className="toolUsed"><h3>React</h3><img src="/images/React.png"></img></div>
+      <div className="toolUsed"><h3>JQuery</h3><img src="/images/JQuery.png"></img></div>
+      <div className="toolUsed"><h3>EJS</h3><img src="/images/ejs.png"></img></div>
+      <div className="toolUsed"><h3>Node.js</h3><img src="/images/nodejs.png"></img></div>
+      <div className="toolUsed"><h3>Express.js</h3><img src="/images/expressjs.png"></img></div>
+      <div className="toolUsed"><h3>APIs/Rest Apis</h3><img src="/images/api.png"></img></div>
+      <div className="toolUsed"><h3>SQL</h3><img src="/images/Sql.png"></img></div>
+      <div className="toolUsed"><h3>PostgreSQL</h3><img src="/images/postgresql.png"></img></div>
+      <div className="toolUsed"><h3>BOOTSTRAP</h3><img src="/images/bootstrap.png"></img></div>
+      <div className="toolUsed"><h3>C++</h3><img src="/images/Cplus.png"></img></div>
+      <div className="toolUsed"><h3>C#</h3><img src="/images/CSharp.png"></img></div>
+      <div className="toolUsed"><h3>OOP</h3><img src="/images/OOP.png"></img></div>
+      <div className="toolUsed"><h3>Unity</h3><img src="/images/unity.png"></img></div>
+      <div className="toolUsed"><h3>Git</h3><img src="/images/Git.png"></img></div>
         </div>
         </div>
         
