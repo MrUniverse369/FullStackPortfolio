d6caca1c86c88010ad58456fbb39ab86d5444a4a
MrUniverse369 <132138248+MrUniverse369@users.noreply.github.com>
2024-09-29 15:41:59 +0100

    mobile responsiveness updated

--- FullStackPortfolio/src/Project.jsx
+++ FullStackPortfolio/src/Project.jsx
@@ -2,13 +2,15 @@ import './Project.css'
 function Project(props){return(
 
  <div className='ProjectsContainer' id='projects'>
- <h2>Projects</h2>
+ <h2><u>Projects</u></h2>
  <div className='ProjectItemOne'>
  <img  className='projectImg' src='/images/Xylo.png'></img>
 <div><h5>Xylo, website.</h5>
   <p>Xylo is a fullstack  website for a Fintech, featuring RESTful APIs and crud operations. The site allows users to  create accounts, shop for payment hardware and software, and request customer service/sales team support via form submission. This was a solo project in which I personally implemented EJS, CSS (using Flexbox and Grid), JavaScript,Node.js Express.js REST APIs, and SQL managed through postgresql to deliver a user-friendly, responsive, and visually appealing web experience</p>
+  <div className='btnCon'>
      <a  href='https://xylo.ltd/' target="_blank"  className='btn'>Live Demo</a>
      <a  href='https://github.com/MrUniverse369/Xylo' target="_blank" className='btn'>GitHub</a>
+     </div>
 </div>
 
 </div>
@@ -17,8 +19,10 @@ function Project(props){return(
 <img className='projectImg' src='/images/Azure.png'></img>
 <div><h5>Azure Horizon, website.</h5>
   <p>Azure Horizon is a hotel website featuring room booking, event planning, and hotel information. Personally executed HTML, CSS, JavaScript, and Bootstrap to deliver a user-friendly, responsive, and visually appealing web experience.</p>
+  <div className='btnCon'>
      <a  href='https://mruniverse369.github.io/AzureHorizon/index.html' target="_blank"  className='btn'>Live Demo</a>
      <a  href='https://github.com/MrUniverse369/AzureHorizon' target="_blank" className='btn'>GitHub</a>
+     </div>
 </div>
 </div>
 
@@ -27,8 +31,10 @@ function Project(props){return(
 <div><h5>Set8Jams, website.</h5>
   <p>Set8 Jams is the website for my indie game studio. Developed using React, the site is  a single-page React application that provides users with detailed insights into the games I'm working on, their current status, and access to where they play them.
 </p>
+<div className='btnCon'>
      <a href='https://set8jams.com/' target="_blank" className='btn' >Live Demo</a>
      <a href='https://github.com/MrUniverse369/Set8JamsReactWeb' target="_blank" className='btn'>GitHub</a>
+     </div>
 </div>
  </div>
 
@@ -39,8 +45,10 @@ function Project(props){return(
 <div><h5>Harvey's Nine Lives, Game.</h5>
   <p>Harveys nine lives is a 2d platform where you step into the paws of a feline archaeologist navigating treacherous tombs whilst doing your best to avoid any traps to get to the hidden treasures these temples hold. I developed the game using C#, Unity and Aseprite.
 </p>
+<div className='btnCon'>
  <a href='https://thamgamedevs.itch.io/haveys-nine-lives' target="_blank" className='btn' >Live Demo</a>
      <a href='https://github.com/MrUniverse369/AnalogueWay' target="_blank" className='btn'>GitHub</a>
+     </div>
 </div>
  </div>
 
@@ -48,8 +56,10 @@ function Project(props){return(
 <img className='projectImg' src='/images/GTCCover.png'></img>
 <div><h5>Galactic crusade, Game.</h5>
   <p>Galactic Crusade is a top down shoot 'em up where you are the pilot of a spaceship that must protect earth from a space colonization by shooting down all the enemies before they comprise the planet's shields. I created this game from scratch using C# Unity OOP principles and Aseprite</p>
+  <div className='btnCon'>
   <a href='https://thamgamedevs.itch.io/galactic-crusade' target="_blank" className='btn' >Live Demo</a>
      <a href='https://github.com/MrUniverse369/GalacticCrusade' target="_blank" className='btn'>GitHub</a>
+     </div>
 </div>
  </div>
 
@@ -57,7 +67,9 @@ function Project(props){return(
 <img className='projectImg' src='/images/BattleShip.png'></img>
 <div><h5>BatttleShip, C++ Console application.</h5>
   <p className=''>A digital recreation of the board game battleship using c++ and the STL Library</p>
+  <div className='btnCon'>
   <a href='https://github.com/MrUniverse369/BattleShip' target="_blank" className='btn'>GitHub</a>
+  </div>
 </div>
  </div>
 
@@ -65,7 +77,9 @@ function Project(props){return(
 <img className='projectImg' src='/images/SnakesAndLadders.png'></img>
 <div><h5>Snakes and Ladders, C++ Console application.</h5>
   <p className=''>A digital recreation of the board game Snake using c++ and the STL Library</p>
+  <div className='btnCon'>
   <a href='https://github.com/MrUniverse369/SnakesandLadders' target="_blank" className='btn' >GitHub</a>
+  </div>
 </div>
  </div>
 
